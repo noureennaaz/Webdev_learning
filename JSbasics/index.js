e3e42204a339f2b52091070007403a50f1362cf2
noureennaaz <noureennaaz09@gmail.com>
2023-03-05 11:38:47 +0530

    Begin Js

---
+++ JSbasics/index.js
@@ -0,0 +1,106 @@
+// Making variables
+
+
+// let a=12;
+// console.log(a);
+
+
+// a='new value';
+// console.log(a);
+
+
+//reference types 
+
+// Arr=['1', 2 , 6 , 3.5];
+
+// for(Element of Arr)
+// {
+//     console.log(Element);
+// }
+
+// console.log(Arr);
+
+
+
+//Making functions
+
+// function fun()
+// {
+//     console.log('Function called');
+// }
+
+// fun();
+
+
+
+
+//Creating objects 
+
+
+// let bag={
+//     pencilBox: 1,
+//     notebook:6,
+//     printThings()
+//     {
+//         console.log('This is not empty');
+        
+//     }
+// }
+
+
+//Creating factory function 
+
+
+// function createPlane(x , y)
+// {
+//     let plane=
+//     {
+//         x,
+//         y,
+
+//         printingfx: function() 
+//         {
+//              console.log(x);
+//              console.log(y);
+//         }
+//     }
+//     return plane;
+// }
+
+// let plane1=createPlane(2 , 3);
+// plane1.printingfx();
+
+// let plane2=createPlane(7 , 9);
+// plane2.printingfx();
+
+
+
+//CREATING CONSTRUCTOR FUNCTION
+
+//there's use of this keyword and no need to return objects
+
+// function createPlane(x ,y )
+// {
+//     x,
+//     y,
+
+//     this.printingfx=function() 
+//         {
+//              console.log(x);
+//              console.log(y);
+//         }
+
+// }
+
+// plane=new createPlane(2, 5);
+// plane.printingfx();
+
+
+//Calling  previously constructor
+
+//This works in console 
+// plane.constructor();
+
+
+
+
